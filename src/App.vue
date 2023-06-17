d217fb13dddaa30b799c647aefd22d5003648bf3
Dan Wu <71037659+wuda20@users.noreply.github.com>
2023-06-17 19:19:32 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <DemoInput>
+  <!-- <HelloWorld msg="Hello Vue 3 in CodeSandbox!" /> -->
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+import DemoInputVue from "./components/DemoInput.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+    DemoInput: DemoInputVue,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DemoInput.vue
@@ -0,0 +1,6 @@
+<template>
+  <form>
+    <h1></h1>
+    <input placeholder="enter here" />
+  </form>
+</template>
